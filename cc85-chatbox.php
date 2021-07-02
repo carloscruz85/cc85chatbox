4f888e2be6d2e2ccaca7f2ddf63a1840d4777f20
carloscruz85 <>
2021-07-02 13:25:18 -0600

    init

---
+++ cc85-chatbox.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: cc85 Chatbox
+Plugin URI: https://github.com/carloscruz85/cc85chatbox
+Description: chatbox made with react
+Version: 1.0.0
+Author: Carlos Cruz
+Author URI: https://github.com/carloscruz85/cc85chatbox
+License: GPLv2 or later
+Text Domain: cc85
+*/
