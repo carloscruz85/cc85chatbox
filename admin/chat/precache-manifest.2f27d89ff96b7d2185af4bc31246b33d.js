ecf3eccd3a1ad83f6b18063802dc284153fac83f
carloscruz85 <carloscruz85@gmail.com>
2021-07-16 15:05:34 -0600

    Changes from server, saved changes

---
+++ admin/chat/precache-manifest.2f27d89ff96b7d2185af4bc31246b33d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9342f3eb8724e8f11d2d9afaf7236006",
+    "url": "./wp-content/plugins/cc85-chatbox/chat/index.html"
+  },
+  {
+    "revision": "2c3691120500fdbe90d9",
+    "url": "./wp-content/plugins/cc85-chatbox/chat/static/css/main.629bd338.chunk.css"
+  },
+  {
+    "revision": "ffebeb7821eac51732e1",
+    "url": "./wp-content/plugins/cc85-chatbox/chat/static/js/2.fc3638b0.chunk.js"
+  },
+  {
+    "revision": "4ba1a1c11c57ed15aa68008707a3d6b4",
+    "url": "./wp-content/plugins/cc85-chatbox/chat/static/js/2.fc3638b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c3691120500fdbe90d9",
+    "url": "./wp-content/plugins/cc85-chatbox/chat/static/js/main.d4c40001.chunk.js"
+  },
+  {
+    "revision": "477b7a93ebea4197a489",
+    "url": "./wp-content/plugins/cc85-chatbox/chat/static/js/runtime-main.f2fb2dad.js"
+  },
+  {
+    "revision": "0a2b77e3662b5b552f5c68ce107190ff",
+    "url": "./wp-content/plugins/cc85-chatbox/chat/static/media/bot.0a2b77e3.png"
+  },
+  {
+    "revision": "ec626edc588fd3487473042ddd33fe99",
+    "url": "./wp-content/plugins/cc85-chatbox/chat/static/media/chat.ec626edc.png"
+  }
+]);
